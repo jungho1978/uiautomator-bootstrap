1a73d69971d90d04cf2b1338c88500789adc051e
권정호 <jungho@gwonjeonghoui-MacBook-Pro.local>
2016-03-15 22:11:51 +0900

    sourcece upload

---
+++ src/com/goodluck/bootstrap/Bootstrap.java
@@ -0,0 +1,8 @@
+package com.goodluck.bootstrap;
+
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
+public class Bootstrap extends UiAutomatorTestCase {
+    public void testRunServer() {
+    }
+}
